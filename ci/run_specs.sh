95586aaf484073a074edbb5138f4b8cd928d813d
Aram Price <aprice@pivotal.io>
2015-03-24 17:07:29 -0700

    `--workdir` must be an absolute path

--- ci/run_specs.sh
+++ ci/run_specs.sh
@@ -6,6 +6,6 @@ echo "-----> Running script: $0"
 docker run \
   --rm=true \
   --volume=${PWD}:/vm_shepherd \
-  --workdir=vm_shepherd \
+  --workdir=/vm_shepherd \
   ${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME} \
   /bin/sh -c 'bundle && bundle exec rspec --format documentation'

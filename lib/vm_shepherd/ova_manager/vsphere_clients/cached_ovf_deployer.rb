1b55c6c1775f0fa22ecb0522823b8c9b275df02c
Aram Price and Whitney Schaefer <aprice@pivotal.io>
2015-04-02 14:22:27 -0700

    Ruby-ify CachedOvfDeployer [#88005306]

--- lib/vm_shepherd/ova_manager/vsphere_clients/cached_ovf_deployer.rb
+++ lib/vm_shepherd/ova_manager/vsphere_clients/cached_ovf_deployer.rb
@@ -6,12 +6,6 @@ module VsphereClients
   class CachedOvfDeployer
     attr_reader :resource_pool, :cluster
 
-    # Constructor. Gets the VIM connection and important VIM objects
-    # @param connection [VIM] VIM Connection
-    # @param network [VIM::Network] Network to attach templates and VMs to
-    # @param cluster [VIM::ComputeResource] Host/Cluster to deploy templates/VMs to
-    # @param folder [VIM::Folder] Folder in which all templates are kept
-    # @param datastore [VIM::Folder] Datastore to store template/VM in
     def initialize(connection, network, cluster, resource_pool, folder, datastore)
       @connection = connection
       @network = network
@@ -32,9 +26,6 @@ module VsphereClients
     # @param template_name [String] Name of the template to be used. Should be the
     #                               same name for the same URL. A cluster specific
     #                               post-fix will automatically be added.
-    # @option opts [int]  :run_without_interruptions Whether or not to disable
-    #                                                SIGINT and SIGTERM during
-    #                                                the OVF upload.
     # @return [VIM::VirtualMachine] The template as a VIM::VirtualMachine instance
     def upload_ovf_as_template(ovf_url, template_name)
       # The OVFManager expects us to know the names of the networks mentioned
@@ -73,70 +64,23 @@ module VsphereClients
         fail 'No host in the cluster available to upload OVF to'
       end
 
-      puts "Uploading OVF to #{host_properties_by_host[found_host]['name']}... @ #{DateTime.now}"
-      property_mappings = {}
+      puts "Uploading OVF to #{host_properties_by_host[found_host]['name']} @ #{DateTime.now}"
 
-      # To work around the VMFS 8-host limit (existed until ESX 5.0), as
-      # well as just for organization purposes, we create one template per
-      # cluster. This also provides us with additional isolation.
-      vm_name = "#{template_name}-#{@cluster.name}"
+      vm =
+        @connection.serviceContent.ovfManager.deployOVF(
+          uri: ovf_url,
+          vmName: "#{template_name}-#{@cluster.name}",
+          vmFolder: @folder,
+          host: found_host,
+          resourcePool: resource_pool,
+          datastore: @datastore,
+          networkMappings: network_mappings,
+          propertyMappings: {},
+        )
+      vm.add_delta_disk_layer_on_all_disks
+      vm.MarkAsTemplate
 
-      vm = nil
-      wait_for_template = false
-      # If the user sets opts[:run_without_interruptions], we will block
-      # signals from the user (SIGINT, SIGTERM) in order to not be interrupted.
-      # This is desirable, as other threads depend on this thread finishing
-      # its prepare job and thus interrupting it has impacts beyond this
-      # single thread or process.
-      run_without_interruptions do
-        begin
-          vm =
-            @connection.serviceContent.ovfManager.deployOVF(
-              uri: ovf_url,
-              vmName: vm_name,
-              vmFolder: @folder,
-              host: found_host,
-              resourcePool: resource_pool,
-              datastore: @datastore,
-              networkMappings: network_mappings,
-              propertyMappings: property_mappings,
-            )
-        rescue RbVmomi::Fault => fault
-          # If two threads execute this script at the same time to upload
-          # the same template under the same name, one will win and the other
-          # with be rejected by VC. We catch those cases here, and handle
-          # them by waiting for the winning thread to finish preparing the
-          # template, see below ...
-          is_duplicate = fault.fault.is_a?(RbVmomi::VIM::DuplicateName)
-          is_duplicate ||= (fault.fault.is_a?(RbVmomi::VIM::InvalidState) &&
-            !fault.fault.is_a?(RbVmomi::VIM::InvalidHostState))
-          if is_duplicate
-            wait_for_template = true
-          else
-            raise fault
-          end
-        end
-
-        # The winning thread succeeded in uploading the OVF. Now we need to
-        # prepare it for (linked) cloning and mark it as a template to signal
-        # we are done.
-        if !wait_for_template
-          vm.add_delta_disk_layer_on_all_disks
-          if opts[:config]
-            # XXX: Should we add a version that does retries?
-            vm.ReconfigVM_Task(spec: opts[:config]).wait_for_completion
-          end
-          vm.MarkAsTemplate
-        end
-      end
-
-      # The losing thread now needs to wait for the winning thread to finish
-      # uploading and preparing the template
-      if wait_for_template
-        puts "Template already exists, waiting for it to be ready @ #{DateTime.now}"
-        vm = wait_for_template_ready(@folder, vm_name)
-        puts "Template fully prepared and ready to be cloned @ #{DateTime.now}"
-      end
+      puts "Marked VM as Template @ #{DateTime.now}"
 
       vm
     end
@@ -171,51 +115,6 @@ module VsphereClients
           config: config,
         }
       ).wait_for_completion
-    end
-
-    private
-
-    # Internal helper method that executes the passed in block while disabling
-    # the handling of SIGINT and SIGTERM signals. Restores their handlers after
-    # the block is executed.
-    def run_without_interruptions
-      int_handler = Signal.trap('SIGINT', 'IGNORE')
-      term_handler = Signal.trap('SIGTERM', 'IGNORE')
-
-      yield
-
-      Signal.trap('SIGINT', int_handler)
-      Signal.trap('SIGTERM', term_handler)
-    end
-
-    # Internal helper method that waits for a template to be fully created. It
-    # polls until it finds the VM in the inventory, and once it is there, waits
-    # for it to be fully created and marked as a template. This function will
-    # block for forever if the template never gets created or marked as a
-    # template.
-    # @param vm_folder [VIM::Folder] Folder in which we expect the template to show up
-    # @param vm_name [String] Name of the VM we are waiting for
-    # @return [VIM::VirtualMachine] The VM we were waiting for when it is ready
-    def wait_for_template_ready(vm_folder, vm_name)
-      vm = nil
-      while !vm
-        sleep 3
-        # XXX: Optimize this
-        vm = vm_folder.children.find { |x| x.name == vm_name }
-      end
-      puts "Template VM found @ #{DateTime.now}"
-      sleep 2
-      loop do
-        runtime, template = vm.collect 'runtime', 'config.template'
-        ready = runtime && runtime.host && runtime.powerState == 'poweredOff'
-        ready = ready && template
-        if ready
-          break
-        end
-        sleep 5
-      end
-
-      vm
     end
   end
 end

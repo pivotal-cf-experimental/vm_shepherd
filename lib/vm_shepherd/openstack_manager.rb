125dc2f8ad59dfd55d353548723e27cd901edbbf
Caroline Taymor <ctaymor@pivotal.io>
2016-04-27 15:05:46 -0700

    Retry openstack cleanup if substeps timeout or hit retry limit
    * Also, wait for volumes to be detached before destroying servers on
      Openstack
    
    Signed-off-by: Derek Richard <drichard@pivotal.io>

--- lib/vm_shepherd/openstack_manager.rb
+++ lib/vm_shepherd/openstack_manager.rb
@@ -8,41 +8,41 @@ module VmShepherd
     def initialize(auth_url:, username:, api_key:, tenant:)
       @auth_url = auth_url
       @username = username
-      @api_key  = api_key
-      @tenant   = tenant
+      @api_key = api_key
+      @tenant = tenant
     end
 
     def deploy(raw_file_path, vm_options)
       say "Uploading the image #{raw_file_path}"
       image = image_service.images.create(
-        name:             "#{vm_options[:name]} #{Time.now}",
-        size:             File.size(raw_file_path),
-        disk_format:      'raw',
+        name: "#{vm_options[:name]} #{Time.now}",
+        size: File.size(raw_file_path),
+        disk_format: 'raw',
         container_format: 'bare',
-        location:         raw_file_path,
+        location: raw_file_path,
       )
       say 'Finished uploading the image'
 
-      flavor          = find_flavor(vm_options[:flavor_name])
-      network         = network_service.networks.find { |net| net.name == vm_options[:network_name] }
+      flavor = find_flavor(vm_options[:flavor_name])
+      network = network_service.networks.find { |net| net.name == vm_options[:network_name] }
       security_groups = vm_options[:security_group_names]
 
       say('Launching an instance')
       server = service.servers.create(
-        name:            vm_options[:name],
-        flavor_ref:      flavor.id,
-        image_ref:       image.id,
-        key_name:        vm_options[:key_name],
+        name: vm_options[:name],
+        flavor_ref: flavor.id,
+        image_ref: image.id,
+        key_name: vm_options[:key_name],
         security_groups: security_groups,
-        nics:            [
-                           {net_id: network.id, v4_fixed_ip: vm_options[:private_ip]}
-                         ],
+        nics: [
+          {net_id: network.id, v4_fixed_ip: vm_options[:private_ip]}
+        ],
       )
       server.wait_for { ready? }
       say('Finished launching an instance')
 
       say('Assigning a Public IP to the instance')
-      ip        = service.addresses.find { |address| address.instance_id.nil? && address.ip == vm_options[:public_ip] }
+      ip = service.addresses.find { |address| address.instance_id.nil? && address.ip == vm_options[:public_ip] }
       ip.server = server
       say('Finished assigning a Public IP to the instance')
     end
@@ -73,45 +73,50 @@ module VmShepherd
     def clean_environment
       say("Destroying #{service.servers.size} instances:")
       retry_until(retry_limit: 2) do
-        service.servers.each do |server|
-          server.volumes.each do |volume|
-            say("  Detaching volume #{volume.id} from server #{server.id}")
-            server.detach_volume(volume.id)
+        begin
+          service.servers.each do |server|
+            server.volumes.each do |volume|
+              say("  Detaching volume #{volume.id} from server #{server.id}")
+              server.detach_volume(volume.id)
+              volume.wait_for { volume.ready? }
+            end
+
+            say("  Destroying instance #{server.id}")
+            server.destroy
           end
 
-          say("  Destroying instance #{server.id}")
-          server.destroy
-        end
+          retry_until(retry_limit: 15) do
+            server_count = service.servers.count
+            say("  Waiting for #{server_count} servers to be destroyed")
+            server_count == 0
+          end
 
-        retry_until(retry_limit: 15) do
-          server_count = service.servers.count
-          say("  Waiting for #{server_count} servers to be destroyed")
-          server_count == 0
-        end
-      end
+          private_images = image_service.images.reject(&:is_public)
 
-      private_images = image_service.images.reject(&:is_public)
+          say("Destroying #{private_images.size} images:")
+          private_images.each do |image|
+            next if image.status == 'deleted'
+            say("  Destroying image #{image.id}")
+            image.destroy
+          end
 
-      say("Destroying #{private_images.size} images:")
-      private_images.each do |image|
-        next if image.status == 'deleted'
-        say("  Destroying image #{image.id}")
-        image.destroy
-      end
+          say("Destroying #{service.volumes.size} volumes:")
+          service.volumes.each do |volume|
+            say("  Destroying volume #{volume.id}; current status: [#{volume.status}]")
+            volume.wait_for { volume.ready? }
+            volume.destroy
+          end
 
-      say("Destroying #{service.volumes.size} volumes:")
-      service.volumes.each do |volume|
-        say("  Destroying volume #{volume.id}; current status: [#{volume.status}]")
-        volume.wait_for { volume.ready? }
-        volume.destroy
-      end
-
-      say("Destroying contents of #{storage_service.directories.size} containers:")
-      storage_service.directories.each do |directory|
-        say("  Destroying #{directory.files.size} files from #{directory.key}")
-        directory.files.each do |file|
-          say("    Destroying file #{file.key}")
-          file.destroy
+          say("Destroying contents of #{storage_service.directories.size} containers:")
+          storage_service.directories.each do |directory|
+            say("  Destroying #{directory.files.size} files from #{directory.key}")
+            directory.files.each do |file|
+              say("    Destroying file #{file.key}")
+              file.destroy
+            end
+          end
+        rescue VmShepherd::RetryHelper::RetryLimitExceeded, Fog::Errors::TimeoutError => e
+          say("Going to retry to cleanup again. First attempt raised #{e.class}: #{e.message}")
         end
       end
     end
@@ -127,22 +132,22 @@ module VmShepherd
 
     def service
       @service ||= Fog::Compute.new(
-        provider:           'openstack',
+        provider: 'openstack',
         openstack_auth_url: auth_url,
         openstack_username: username,
-        openstack_tenant:   tenant,
-        openstack_api_key:  api_key,
+        openstack_tenant: tenant,
+        openstack_api_key: api_key,
         connection_options: connection_options,
       )
     end
 
     def image_service
       @image_service ||= Fog::Image.new(
-        provider:                'openstack',
-        openstack_auth_url:      auth_url,
-        openstack_username:      username,
-        openstack_tenant:        tenant,
-        openstack_api_key:       api_key,
+        provider: 'openstack',
+        openstack_auth_url: auth_url,
+        openstack_username: username,
+        openstack_tenant: tenant,
+        openstack_api_key: api_key,
         openstack_endpoint_type: 'publicURL',
         connection_options: connection_options,
       )
@@ -150,22 +155,22 @@ module VmShepherd
 
     def network_service
       @network_service ||= Fog::Network.new(
-        provider:           'openstack',
+        provider: 'openstack',
         openstack_auth_url: auth_url,
         openstack_username: username,
-        openstack_tenant:   tenant,
-        openstack_api_key:  api_key,
+        openstack_tenant: tenant,
+        openstack_api_key: api_key,
         connection_options: connection_options,
       )
     end
 
     def storage_service
       @network_service ||= Fog::Storage.new(
-        provider:           'openstack',
+        provider: 'openstack',
         openstack_auth_url: auth_url,
         openstack_username: username,
-        openstack_tenant:   tenant,
-        openstack_api_key:  api_key,
+        openstack_tenant: tenant,
+        openstack_api_key: api_key,
         connection_options: connection_options,
       )
     end
